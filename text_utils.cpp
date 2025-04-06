9b874b6acc3ec11d6ffaeb0a5934a2f241874662
Nate Medina <nate.medina0820@gmail.com>
2025-04-05 22:22:48 -0500

    added files

--- text_utils.cpp
+++ text_utils.cpp
@@ -5,41 +5,17 @@
 #include <algorithm>  // for std::transform
 
 std::string loadTextFile(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file) {
-        return "";
-    }
 
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
 }
 
 std::string cleanText(const std::string& rawText) {
-    std::string cleaned;
-    for (char c : rawText) {
-        if (std::isalpha(c) || std::isspace(c)) {
-            cleaned += std::tolower(c);
-        }
-        // skip punctuation and symbols
-    }
-    return cleaned;
+
 }
 
 std::vector<std::string> tokenize(const std::string& text) {
-    std::vector<std::string> words;
-    std::istringstream stream(text);
-    std::string word;
-    while (stream >> word) {
-        words.push_back(word);
-    }
-    return words;
+
 }
 
-std::vector<std::pair<std::string, std::string>> generateBigrams(const std::vector<std::string>& words) {
-    std::vector<std::pair<std::string, std::string>> bigrams;
-    for (size_t i = 0; i < words.size() - 1; ++i) {
-        bigrams.emplace_back(words[i], words[i + 1]);
-    }
-    return bigrams;
+std::vector<std::pair<std::string, std::string>> generateBigrams(const std::vector<std::string>& words){ 
+  
 }
