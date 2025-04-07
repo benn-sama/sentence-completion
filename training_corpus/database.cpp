929e89e77b1da021e20805121602c5d007b829cd
benn-sama <green.l.benjamin04@gmail.com>
2025-04-07 16:50:11 -0500

    💥🔥💀 Reconstructed the database from the ground up; functionality is in a state of rebirth. WIP 🛠️

--- training_corpus/database.cpp
+++ training_corpus/database.cpp
@@ -5,32 +5,21 @@ bool Database::insertOneWord(std::string word) { // method for inserting a one w
     return false;
   }
 
-  if (singleWord.find(word) != singleWord.end()) {
-    singleWord[word] += 1;
-  }
-  else {
-    singleWord[word] = 1;
-  }
+  singleWord[word]++;
 
   return true;
 }
 
-bool Database::insertTwoWord(std::string word, std::string sentence) { // method for inserting a two word string
-  if (word.empty()) {
+bool Database::insertTwoWord(std::string firstWord, std::string secondWord) { // method for inserting a two word string
+  if (secondWord.empty()) {
     return false;
   }
 
-  if (doubleWord.find(sentence) != doubleWord.end()) {
-    doubleWord[sentence] += 1;
-  }
-  else {
-    doubleWord[sentence] = 1;
-  }  
-
+  doubleWord[firstWord][secondWord]++;
   return true;
 }
 
-void Database::insert(std::string sentence) { // inserts string
+void Database::insert(std::string sentence) { // inserts two word string
   std::istringstream stream(sentence);
   std::string firstWord = "";
   std::string secondWord = "";
@@ -41,20 +30,51 @@ void Database::insert(std::string sentence) { // inserts string
     return;
   }
 
-  if (!this->insertTwoWord(secondWord, sentence)) {
+  if (!this->insertTwoWord(firstWord, secondWord)) {
     return;
   }
 }
 
-int Database::retrieve(std::string sentence) { // returns total string appearance or returns -1
-  if (singleWord.find(sentence) != singleWord.end()) {
-    return singleWord[sentence];
+int Database::retrieveCount(std::string sentence) { // returns total string appearance or returns -1
+  std::istringstream stream(sentence);
+  std::string firstWord = "";
+  std::string secondWord = "";
+
+  stream >> firstWord >> secondWord;
+
+  if (firstWord.empty()) {
+    return -1;
   }
 
-  if (doubleWord.find(sentence) != doubleWord.end()) {
-    return doubleWord[sentence];
+  if (secondWord.empty()) {
+    return singleWord[firstWord];
+  }
+  else {
+    return doubleWord[firstWord][secondWord];
   }
 
   return -1;
+}
+
+std::string Database::retrieveNextWord(std::string word) { // retrieves the next word
+  std::unordered_map<std::string, std::unordered_map<std::string, int>>::const_iterator outerTable;
+  outerTable = doubleWord.find(word);
+
+  
+  if (outerTable == doubleWord.end()) {
+    return "";
+  }
+
+  std::unordered_map<std::string, int>::const_iterator innerTable;
+  int max = 0;
+  std::string nextWord = "";
+  for (innerTable = outerTable->second.begin(); innerTable != outerTable->second.end(); ++innerTable) {
+    if (innerTable->second > max) {
+      max = innerTable->second;
+      nextWord = innerTable->first;
+    }
+  }
+
+  return nextWord;
 }
 
--- training_corpus/database.hpp
+++ training_corpus/database.hpp
@@ -24,15 +24,16 @@
 class Database {
   private:
     std::unordered_map<std::string, int> singleWord; // stores single words
-    std::unordered_map<std::string, int> doubleWord; // stores double words
+    std::unordered_map<std::string, std::unordered_map<std::string, int>> doubleWord; // stores double words
     
     bool insertOneWord(std::string word);
-    bool insertTwoWord(std::string word, std::string sentence);
+    bool insertTwoWord(std::string firstWord, std::string secondWord);
   public:
     Database() {};
     Database(int reserveCount) { singleWord.reserve(reserveCount); doubleWord.reserve(reserveCount); }
     void insert(std::string sentence);  // inserts string based on whether it is a one word or a two word
-    int retrieve(std::string sentence); // retrieves string
+    int retrieveCount(std::string sentence); // retrieves string
+    std::string retrieveNextWord(std::string word); // retrieves the next word
 };
 
 #endif
